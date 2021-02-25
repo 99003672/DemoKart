755211367834d6aa4858e9226f1e5e86e8ac25df
99003672 <78850143+99003672@users.noreply.github.com>
2021-02-25 23:59:57 +0530

    Add files via upload

---
+++ DemoKartBLTests/UserTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DemoKartBL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoKartBL.Tests
+{
+    [TestClass()]
+    public class UserTests
+    {
+        [TestMethod()]
+        public void LoginTest()
+        {
+            User uObj = new User();
+            uObj.Login("shrishti", "shri");
+            Assert.AreEqual(1,1);
+        }
+    }
+}
